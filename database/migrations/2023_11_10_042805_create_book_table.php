024139ed95a2fce3725508c71aad912628e862d9
Eduardo Jimenez <95043592+EJPZZZ@users.noreply.github.com>
2023-11-09 22:37:43 -0600

    Creadas las tblas de admin, book, report, circle y sus relaaciones

---
+++ database/migrations/2023_11_10_042805_create_book_table.php
@@ -0,0 +1,32 @@
+book 
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book', function (Blueprint $table){
+            $table->id();
+            $table->string('author')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('publication_year')->nullable();
+            $table->string('genre')->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
