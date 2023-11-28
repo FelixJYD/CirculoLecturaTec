3345737a5ad8026cef9ce6e22779565099ec0592
Daniel <batalladc5@gmail.com>
2023-11-28 10:43:46 -0600

    Se agrego el Footer a la vista de Welcome

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="bg-white py-8 flex flex-col md:flex-row items-center">
+      <!-- Información -->
+      <div class="container mx-auto w-full md:w-1/3">
+
+        <div class="flex items-center">
+            <p class="text-lg text-black font-extrabold pl-8 md:pl-20">
+                Información
+            </p>
+        </div>
+        <div class="flex items-center">
+          <i class="fa fa-location-arrow text-blue-600 text-2xl mr-2 pl-20"></i>
+          <p class="text-lg text-black font-normal pl-2 md:pl-0">Carretera Teapa-Tacotalpa Km 4.5, Francisco Javier Mina, Teapa Tabasco C.P. 86801</p>
+        </div>
+        <div class="flex items-center mt-2">
+          <i class="fa fa-phone text-2xl text-blue-500 mr-2 pl-20"></i>
+          <p class="text-lg text-black font-normal pl-2 md:pl-0">Teléfono:       9323 240 650</p>
+        </div>
+        <div class="flex items-center mt-2">
+          <i class="fa fa-envelope text-2xl text-blue-500 mr-2 pl-20"></i>
+          <p class="text-lg text-black font-normal pl-2 md:pl-0">Email: regionsierra@regionsierra.tecnm.mx direccion@regionsierra.tecnm.mx</p>
+        </div>
+      </div>
+  
+      <!-- Redes Sociales -->
+      <div class="container mx-auto w-full md:w-1/3 pl-36">
+        <a href="https://www.facebook.com/TecNMRSierra" target="_blank" rel="noopener noreferrer" class="flex items-center">
+        <i class="fab fa-facebook-square text-2xl text-blue-600 mr-2"></i>
+        <p class="text-lg text-black font-normal pl-2 md:pl-0">@TECNMRSIERRA</p>
+      </a>
+      <a href="HTTP://WWW.ITSS.EDU.MX/" target="_blank" rel="noopener noreferrer" class="flex items-center mt-2">
+        <i class="fa fa-link text-2xl text-blue-400 mr-2"></i>
+        <p class="text-lg text-black font-normal pl-2 md:pl-0">HTTP://WWW.ITSS.EDU.MX/</p>
+      </a>
+      </div>
+  
+      <!-- Logo -->
+      <div class="container mx-auto w-full md:w-1/3 pl-56">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/d/d4/Logo-TecNM-2017.png" class="w-64 h-auto md: pr-0 pt-2">
+      </div>
+    </div>
+  </template>
+
+
+
+<style>
+
+</style>
